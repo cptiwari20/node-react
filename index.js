9fedb01690b8d0a4de6278245ed614652fd58713
cptiwari20 <cptiwari20@gmail.com>
2018-08-03 12:02:33 +0530

    adding passport auth routes and middlewares

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const passport = require('passport');
-const GoogleStretegy = require('passport-google-oauth20').Strategy;
+const cookieSession = require('cookie-session');
 
 const keys = require('./config/keys')
 
+require('./models/User')
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 const app = express();
 
-passport.use(new GoogleStretegy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: '/auth/google/callback'
-    },
-    (accessToken, refreshToken, profile, done) => {
-        console.log('access Token:', accessToken);
-        console.log('refresh Token:', refreshToken);
-        console.log('Profile#####', profile);    
-    }
-));
+app.use( 
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
+require('./routes/authRoutes')(app);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
