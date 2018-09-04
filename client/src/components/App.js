2ab86522b6ac602d64e6a6d77c2803e753955403
cptiwari20 <cptiwari20@gmail.com>
2018-09-04 19:05:37 +0530

    developing client side

---
+++ client/src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <h3 className='text-center text-info'>This is my first react Application</h3>
+                
+            </div>
+        )
+    }
+}
+
+
+export default App;
