85577746a90f6e5adce777cd479e1985d30d8a53
cptiwari20 <cptiwari20@gmail.com>
2018-09-20 20:48:37 +0530

    added sengrid apis on backend

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const passport = require('passport');
 const cookieSession = require('cookie-session');
 const bodyParser = require('body-parser')
 
-const keys = require('./config/keys')
-
-require('./models/User')
+// requiring some addition files
+require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+const keys = require('./config/keys')
+
+// Middlewares
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use( 
     cookieSession({
@@ -22,9 +25,13 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// Routes
 require('./routes/authRoutes')(app);
 require('./routes/paymentRoutes')(app);
+require('./routes/surveyRoutes')(app);
 
+
+// Production routes
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     const path = require('path');
@@ -33,6 +40,7 @@ if(process.env.NODE_ENV === 'production'){
     })
 };
 
+// Hosting port
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server has been start on Port: ${port}`);
---
+++ routes/surveyRoutes.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+const requiresLogin = require('../middlewares/requiresLogin');
+const requiresCredits = require('../middlewares/requiresCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
+
+const Survey = mongoose.model('surveys')
+
+module.exports = app => {
+
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for your feedback!')
+    })
+
+    app.post('/api/surveys', requiresLogin, requiresCredits, async (req, res) => {
+        const { title, body, subject, recipients, _user } = req.body;
+
+        const survey = new Survey({
+            title,
+            body,
+            subject,
+            recipients: recipients.split(',').map(email => ({email: email.trim()})),
+            _user: req.user.id,
+            sentDate: Date.now()
+        });
+
+        const mailer = new Mailer(survey, surveyTemplate(survey));
+        try{
+            await mailer.send();
+            await survey.save();
+            req.user.credits -= 1;
+            const user = await req.user.save();
+
+            res.send(user);
+        }catch (err) {
+            //unprocessable intity or something wrong with the data you sent us.
+            res.status(422).send(err)
+        }
+    })
+}
