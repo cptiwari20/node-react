f0db2643a4acd0488f6a55028ec65d531e92338a
cptiwari20 <cptiwari20@gmail.com>
2018-09-24 22:25:16 +0530

    sg webhook routes before refactoring

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,6 @@
 const { URL } = require('url');
-const Path = require('path-parser')
+const _ = require('lodash')
+const Path = require('path-parser').default;
 const mongoose = require('mongoose')
 const requiresLogin = require('../middlewares/requiresLogin');
 const requiresCredits = require('../middlewares/requiresCredits');
@@ -19,9 +20,18 @@ module.exports = app => {
             const pathname = new URL(url).pathname;
             const p = new Path('/api/surveys/:surveyId/:choice');
             const match = p.test(pathname);
-            console.log(match);
-            
-        })
+            if(match){
+                return { email, surveyId: match.surveyId, choice: match.choice};
+            };
+        });
+        const compactEvents = _.compact(events); // removes undefined or null
+        const uniqueEvents = _.uniqBy(compactEvents, 'surveyId', 'email');
+
+
+        console.log(uniqueEvents);
+        console.log('This is it ***************')
+
+        res.send({})
     })
 
     app.post('/api/surveys', requiresLogin, requiresCredits, async (req, res) => {
--- sendgrid_webhook.js
+++ sendgrid_webhook.js
@@ -1,4 +1,7 @@
 var localtunnel = require('localtunnel');
 localtunnel(5000, { subdomain: 'vikasddfszziziziz' }, function(err, tunnel) {
-  console.log('LT running')
+  if(err){
+    console.log(err)
+  }
+  console.log('LT running:##########')
 });
