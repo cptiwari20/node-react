94cd2165c943879971944148f2de3af8863f05f3
cptiwari20 <cptiwari20@gmail.com>
2018-09-25 13:51:07 +0530

    added webhook data to mongodb

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -18,7 +18,7 @@ module.exports = app => {
         const data = req.body;
         const p = new Path('/api/surveys/:surveyId/:choice');
 
-        const events = _.chain(data)
+        _.chain(data)
             .map(({ url, email}) => {
                 const match = p.test(new URL(url).pathname);
                 if(match){
@@ -27,11 +27,19 @@ module.exports = app => {
             })
             .compact()
             .uniqBy('surveyId', 'email')
+            .each(({ surveyId, email, choice }) => {
+                Survey.updateOne(
+                {
+                    _id: surveyId,
+                    recipients: {
+                        $elemMatch : { email: email, responded: false}
+                }
+                }, {
+                    $inc: { [choice]: 1},
+                    $set: { 'recipients.$.responded': true }
+                }).exec();
+            })
             .value();
-
-
-        console.log(events);
-        console.log('This is it ***************')
 
         res.send({})
     })
